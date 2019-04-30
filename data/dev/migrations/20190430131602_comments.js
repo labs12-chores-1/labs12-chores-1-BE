51eee283b17c481b05f91489d8e0a38c6b4d418f
Tsai Huang <tsaihuangsd@gmail.com>
2019-04-30 14:31:05 -0700

    created comments migration table

---
+++ data/dev/migrations/20190430131602_comments.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('comments', (table) => {
+        // initialize comments table db rows
+
+        //auto-increment primary key for table i.e. comment ID
+        table.increments('id');
+        table.string('commentString', 255).notNullable();
+        table.integer('taskID').notNullable();
+        table.integer('groupID').references('id').inTable('groups').onDelete("CASCADE").notNullable();
+        table.integer('commentedBy');
+
+        // timestamps the moment of user creation (i.e. registration date)
+        table.timestamp('commentedOn').defaultTo(knex.fn.now());
+
+
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('comments');
+};
